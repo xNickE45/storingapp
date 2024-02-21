d8839acca8a7d554cb6a6a53aab5bb82374af518
xNickE45 <159444109+xNickE45@users.noreply.github.com>
2024-02-21 12:05:08 +0100

    update
    ben gekomen bij Inputvalidatie moet ik nog gaan maken

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,8 +2,22 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+if(empty($attractie))
+{ 
+    $errors[]= "Vuldeattractie-naamin.";
+}
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit']; 
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = 1;
+}
+else
+{
+    $prioriteit = 0;
+}
 $melder = $_POST['melder'];
+$overig = $_POST['overig'];
 
 // echo $attractie . " / " . $capaciteit . " / " . $melder;
 
@@ -11,14 +25,17 @@ $melder = $_POST['melder'];
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, capaciteit, melder) VALUES(:attractie, :capaciteit, :melder)";
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 //3. Prepare
 $statement = $conn->prepare($query);
 //4. Execute
 $statement->execute([
     ":attractie" => $attractie,
+    ":type" => $type,
     ":capaciteit" => $capaciteit,
-    ":melder" => $melder
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overig
 ]);
 
 $items = $statement->fetchALL(PDO::FETCH_ASSOC);
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -22,8 +22,16 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <select name="group" id="group">
-                    <option value="">
+                    <select name="type" id="type">
+                        <option value=""> - kies een type - </option>
+                        <option value="achtbaan">Achtbaan</option>
+                        <option value="draaiende">Draaiende attractie</option>
+                        <option value="kinderattractie">kinderattractie</option>
+                        <option value="restaurant">Restaurant, cafe, etc.</option>
+                        <option value="parkshow">Parkshow</option>
+                        <option value="waterattractie">Waterattractie</option>
+                        <option value="overig">Overig</option>
+                    </select>
                 <!-- hier komt een dropdown -->
             </div>
             <div class="form-group">
@@ -31,9 +39,19 @@
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prio</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Melding met prioriteit</label>
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
             </div>
+            <div class="form-group">
+                <label for="overig">Overige info</label>
+                <textarea name="overig"id="overig"class="form-input"rows="4"> </textarea>
+            </div>
+            
 
             <input type="submit" value="Verstuur melding">
 
