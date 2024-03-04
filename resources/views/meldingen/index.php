c351a01273930de46e1119506894213fe58c4d94
xNickE45 <159444109+xNickE45@users.noreply.github.com>
2024-03-04 12:31:39 +0100

    done

--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -20,7 +20,45 @@
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
-        <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
+        <?php 
+            require_once '../../../config/conn.php';
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        ?>
+        <table>
+            <tr>
+                <th>Attractie</th>
+                <th>Type</th>
+                <th>Capaciteit</th>
+                <th>prioriteit</th>
+                <th>Melder</th>
+                <th>Gemeld_op</th>
+                <th>Overige info</th>
+            </tr>
+            <?php foreach($meldingen as $melding): ?>
+                <tr>
+                    <td><?php echo $melding['attractie']; ?></td>
+                    <td><?php echo $melding['type']; ?></td>
+                    <td><?php if (isset($melding['capaciteit'])) {
+                            echo $melding['capaciteit'];} ?></td>
+                    <td><?php if ($melding['prioriteit'] == 1) {
+                        echo "ja";
+                    } else {
+                        echo "nee";
+                    } ?></td>
+                    <td><?php echo $melding['melder']; ?></td>
+                    <td><?php echo $melding['gemeld_op']; ?></td>
+                    <td><?php echo $melding['overige_info']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
+
+
+        <!-- <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div> -->
     </div>
 
 </body>
